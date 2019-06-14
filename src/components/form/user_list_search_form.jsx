234abc8e462060ab4b9b95a3d3d026d315ef88cb
dongxingbin <dongxingbin@zbj.com>
2019-06-14 18:15:02 +0800

    feat(添加用户列表)

---
+++ src/components/form/user_list_search_form.jsx
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col, Form, Input, Select, Button } from 'antd';
+
+import { formItemLayout } from '../../common/const';
+import { getEnumsArray } from '../../common/tool';
+import UserEnums from '../../enums/user';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class UserListSearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        const { onSubmit, form } = this.props;
+        if(e) e.preventDefault();
+        form.validateFields((err, values) => {
+            if (err) return;            
+            onSubmit(values);
+        });
+    }
+
+    render() {
+        const { form, defaultValue } = this.props;
+        const { getFieldDecorator } = form;
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Row className="m-b">
+                    <Col span={8}>
+                        <FormItem
+                            label='用户ID'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('userId', {
+                                initialValue: defaultValue.userId ? defaultValue.userId : ''
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem
+                            label='用户昵称'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('userName', {
+                                initialValue: defaultValue.userName ? defaultValue.userName : ''
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem
+                            label='性别'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('gender', {
+                                initialValue: defaultValue.gender ? defaultValue.gender : ''
+                            })(
+                                <Select>
+                                    <Option value="">全部</Option>
+                                    {getEnumsArray(UserEnums.gender).map((data, index) => {
+                                        return (<Option value={data.value} key={index}>{data.text}</Option>);
+                                    })}
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row className="m-b">
+                   <Col span={8}>
+                        <FormItem
+                            label='年龄'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('age', {
+                                initialValue: defaultValue.age ? defaultValue.age : ''
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem
+                            label='手机号'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('tel', {
+                                initialValue: defaultValue.tel ? defaultValue.tel : ''
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={8}>
+                        <FormItem
+                            label='创建时间'
+                            {...formItemLayout}
+                        >
+                            {getFieldDecorator('createTime', {
+                                initialValue: defaultValue.createTime ? defaultValue.createTime : ''
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <div className="text-right">
+                        <Button className="m-l" type="primary" htmlType="submit" loading={false}>查询</Button>
+                    </div>
+                </Row>
+            </Form>
+        );
+    }
+}
+
+UserListSearchForm.propTypes = {
+    form: PropTypes.object.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    defaultValue: PropTypes.object
+};
+
+export default Form.create()(UserListSearchForm);
---
+++ src/pages/user_list.jsx
@@ -0,0 +1,98 @@
+/**
+ * 用户列表
+ */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Row, Col, Spin } from 'antd';
+import { push } from 'react-router-redux';
+
+import UserListSearchForm from './../components/form/user_list_search_form.jsx';
+import UserListTable from './../components/table/user_list_table.jsx';
+import { getQuery, isObjEmpty, getPath, serialize, getPageData } from './../common/tool';
+
+function propMap(state, ownProps) {
+    return {
+        modal: state.modal,
+        routing: ownProps,
+        users: []
+    };
+}
+
+let users = [{
+    key: 0,
+    userId: 1,
+    userName: 'zhangsan'
+},
+{
+    key: 1,
+    userId: 2,
+    userName: 'lisi'
+},
+{
+    key: 2,
+    userId: 3,
+    userName: 'wangwu'
+}];
+
+class UserList extends Component {
+    constructor(props) {
+        super(props);
+        this.handleGetList = this.handleGetList.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleTableChange = this.handleTableChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.handleGetList();
+    }
+
+    handleGetList(filters) { // 第二个参数为了确保选择其他业务线的时候清空查询参数避免参数冲突
+        const {dispatch, routing} = this.props;
+        const query = Object.assign({}, getQuery(routing), filters);
+        // dispatch(getStrictSelectionChanceList(query));
+        if(!isObjEmpty(filters)) dispatch(push(getPath('/user/?' + serialize(query))));
+    }
+
+    handleSearch(data) {
+        this.handleGetList(Object.assign({}, data, {
+            page: 1,
+            pageNum: 10
+        }));
+    }
+
+    handleTableChange(page, filters) {
+        this.handleGetList(Object.assign( page ? {
+            page: page.current,
+            pageSize: page.pageSize
+        } : {}, filters));
+    }
+
+    render() {
+        const { routing, modal } = this.props;
+        const { loadingForm } = modal;
+        return (
+            <Spin spinning={loadingForm}>
+                <Row className="m-b">
+                    <Col span={24}>
+                        <h1 className="pull-left">用户列表</h1>
+                    </Col>
+                </Row>
+                <UserListSearchForm onSubmit={this.handleSearch} defaultValue={getQuery(routing)}/>
+                <UserListTable 
+                    dataSource={users}
+                    pagination={false}
+                    onChange={this.handleTableChange}
+                />
+            </Spin>
+        );
+    }
+}
+
+UserList.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    routing: PropTypes.object.isRequired,
+    modal: PropTypes.object.isRequired
+};
+
+export default connect(propMap)(UserList);
