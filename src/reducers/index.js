46ea9a0e3f9e60c1b12c541ffc307341f93f4694
1029278668@qq.com <1029278668@qq.com>
2019-06-16 18:27:19 +0800

    feat(添加用户列表)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,8 +6,10 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import modal from './modal';
+import user from './user';
 
 export default combineReducers({
     modal,
+    user,
     routing: routerReducer
 });
