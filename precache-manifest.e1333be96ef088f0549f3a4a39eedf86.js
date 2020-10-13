bd31bdeee2662239f55f665fd21f3652ec95e819
dongxingbin <dongxingbin@zbj.com>
2020-10-13 17:34:51 +0800

    Updates

---
+++ precache-manifest.e1333be96ef088f0549f3a4a39eedf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6b9edc3599cb0387934",
+    "url": "/antd-admin/static/js/runtime~main.a6b9edc3.js"
+  },
+  {
+    "revision": "54a25507e2ae9c760eb1",
+    "url": "/antd-admin/static/js/main.54a25507.chunk.js"
+  },
+  {
+    "revision": "689f2cd652086015f47d",
+    "url": "/antd-admin/static/js/2.689f2cd6.chunk.js"
+  },
+  {
+    "revision": "54a25507e2ae9c760eb1",
+    "url": "/antd-admin/static/css/main.e02a0b37.chunk.css"
+  },
+  {
+    "revision": "689f2cd652086015f47d",
+    "url": "/antd-admin/static/css/2.aba6d9aa.chunk.css"
+  },
+  {
+    "revision": "0837b2e44f4bc3716c8b8e3d311c41ea",
+    "url": "/antd-admin/index.html"
+  }
+];
