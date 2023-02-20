3281ca3eabcaf4635faf851857d6fb8735200800
artdong <1029278668@qq.com>
2023-02-20 15:19:19 +0800

    feat：add docs

---
+++ docs/precache-manifest.3f57cee49470dbd0a13a507e0f027ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0933e05cc0369bf70d",
+    "url": "./static/css/main.e02a0b37.chunk.css"
+  },
+  {
+    "revision": "ec0933e05cc0369bf70d",
+    "url": "./static/js/main.ec0933e0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "27a269f7c042b38f0e7f",
+    "url": "./static/css/2.1a7cc4b8.chunk.css"
+  },
+  {
+    "revision": "27a269f7c042b38f0e7f",
+    "url": "./static/js/2.27a269f7.chunk.js"
+  },
+  {
+    "revision": "8f649914db156f6ad020dd5d16ff3c7b",
+    "url": "./index.html"
+  }
+];
